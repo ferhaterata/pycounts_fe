b24bfaf72e750f5059777eb299dd6b937e8afe20
Ferhat Erata <ferhat.erata@yale.edu>
2023-09-10 22:41:24 -0400

    formatting

--- src/pycounts_fe/plotting.py
+++ src/pycounts_fe/plotting.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
+
 def plot_words(word_counts, n=10):
     """Plot a bar chart of word counts.
-    
+
     Parameters
     ----------
     word_counts : collections.Counter
